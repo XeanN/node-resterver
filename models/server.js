d55f3c084dfb4afebcbfb6f7a572c1ca901e8dbd
XeanN <angel.xp.pb@gmail.com>
2023-04-13 23:34:11 -0500

    Fin de Subir achivos - Cloudinary

--- models/server.js
+++ models/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const { dbConnection } = require('../database/config');
+const fileUpload = require('express-fileupload');
 
 class Server {
     constructor() {
@@ -12,7 +13,8 @@ class Server {
             buscar:     '/api/buscar',
             categorias: '/api/categorias',
             productos: '/api/productos',
-            usuarios:   '/api/usuarios'
+            usuarios:   '/api/usuarios',
+            uploads:    '/api/uploads'
         }
 
         // Conectar a base de datos
@@ -37,6 +39,14 @@ class Server {
 
         //Directorio Público
         this.app.use(express.static('public'));
+
+        // Fileupload - Carga de archivos
+        this.app.use(fileUpload({
+            useTempFiles: true,
+            tempFileDir: '/tmp/',
+            createParentPath: true
+        }));
+
     }
 
     routes() {
@@ -45,6 +55,7 @@ class Server {
         this.app.use( this.paths.categorias, require('../routes/categorias'));
         this.app.use( this.paths.productos, require('../routes/productos'));
         this.app.use( this.paths.usuarios, require('../routes/user'));
+        this.app.use( this.paths.uploads, require('../routes/uploads'));
     }
 
     listen() {
